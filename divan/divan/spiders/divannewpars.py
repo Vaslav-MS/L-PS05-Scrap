98e7b6cf8467984cb99d82553b8343a21dc6eabd
Vaslav <2key@rambler.ru>
2024-12-05 16:10:43 +0500

    modify svet

--- divan/divan/spiders/divannewpars.py
+++ divan/divan/spiders/divannewpars.py
@@ -4,13 +4,17 @@ import scrapy
 class DivannewparsSpider(scrapy.Spider):
     name = "divannewpars"
     allowed_domains = ["https://divan.ru"]
-    start_urls = ["https://divan.ru/category/divany-i-kresla"]
+    start_urls = ["https://www.divan.ru/ekaterinburg/category/svet"]
 
     def parse(self, response):
-        divans = response.css('div._Ud0k')
-        for divan in divans:
+#       divans = response.css('div._Ud0k')
+        svetis = response.css('div.LlPhw')
+        for svet in svetis:
             yield {
-                'name' : divan.css('div.lsooF span::text').get(),
-                'price': divan.css('div.q5Uds span::text').get(),
-                'url': divan.css('a').attrib['href']
+#               'name' : divan.css('div.lsooF span::text').get(),
+#               'price': divan.css('div.q5Uds span::text').get(),
+#               'url': divan.css('a').attrib['href']
+                'name': svet.css('div.lsooF span::text').get(),
+                'price': svet.css('div.q5Uds span::text').get(),
+                'url': svet.css('a').attrib['href']
             }
